6d418cc0db0e85c8d3d30d960624b1cb76782e6e
Shoaib <muhammadshoaibgopang@gmail.com>
2024-06-27 22:31:33 +0500

    Completed accordion app

--- Questions/src/App.js
+++ Questions/src/App.js
@@ -1,6 +1,39 @@
-import React, { useState } from 'react'
-function App() {
-  
+import React, { useState } from "react";
+import questions from "./data.js";
+import hideicon from "./icons/hide.svg";
+import showicon from "./icons/show.svg";
+
+function Question(variable1) {
+    console.log(variable1.props);
+    const { id,title, info,} = variable1.props;
+    const [showinfo,setinfo] =useState(false)
+
+  return (
+    <div key={id} className="article">
+      <div className="titlediv">
+        <p className="titleparagraph">{title}</p>
+        {showinfo? <img src={showicon} alt="show-icon" className="showhide" onClick={()=>{setinfo(!showinfo)}} />:<img src={hideicon} alt="hide-icon" className="showhide" onClick={()=>{setinfo(!showinfo)}} /> }
+        
+      </div>
+      {showinfo && <p className="infoparagraph">{info}</p>}
+      
+    </div>
+  );
 }
 
-export default App
+function App() {
+    return (
+    <main>
+      <div id="container">
+        <div id="header">Questions</div>
+        {questions.map((question) => {
+            return(
+                <Question props={question} />
+            )
+        })}
+      </div>
+    </main>
+  );
+}
+
+export default App;
